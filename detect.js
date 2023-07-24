70c312fdf51174030558bdced59add22507a5dc9
Robin Dong <rdong@woolworths.com.au>
2023-07-24 13:28:55 +1000

    Could give simplified output, but still lack ratio transform

--- detect.js
+++ detect.js
@@ -5,6 +5,7 @@ const fs = require("fs");
 const modelHeight = 640;
 const modelWidth = 640;
 const modelChannel = 3;
+const CLS_CONF_IDX = 4;
 
 async function preProcess(frame, dstInput) {
   const origData = new Uint8Array(frame.data);
@@ -25,15 +26,7 @@ async function preProcess(frame, dstInput) {
       for (var w = 0; w < modelWidth; ++w) {
         const origW = Math.round(w * wRatio);
 
-        var offset;
-        if (c == 0) {
-          offset = 2;
-        } else if (c == 2) {
-          offset = 0;
-        } else {
-          offset = c;
-        }
-        const origIndex = origHOffset + origW * origWStride + offset;
+        const origIndex = origHOffset + origW * origWStride + c;
 
         var val = origData[origIndex] / 255.0
         dstInput[idx] = val;
@@ -44,25 +37,19 @@ async function preProcess(frame, dstInput) {
   }
 }
 
-function xywh2xyxy(cx1, cy1, w1, h1) {
-  left1 = Math.round(cx1 - w1/2.0);
-  right1 = Math.round(cx1 + w1/2.0);
-  top1 = Math.round(cy1 - h1/2.0);
-  bottom1 = Math.round(cy1 + h1/2.0);
-  console.log("show:", left1, right1, top1, bottom1);
+function xywh2xyxy(cx, cy, width, height) {
+  left = Math.round(cx - width/2.0);
+  right = Math.round(cx + width/2.0);
+  top = Math.round(cy - height/2.0);
+  bottom = Math.round(cy + height/2.0);
+  return [left, right, top, bottom];
 }
 
 function iou(cx1, cy1, w1, h1, cx2, cy2, w2, h2) {
-  left1 = Math.round(cx1 - w1/2.0);
-  right1 = Math.round(cx1 + w1/2.0);
-  top1 = Math.round(cy1 - h1/2.0);
-  bottom1 = Math.round(cy1 + h1/2.0);
+  let [left1, right1, top1, bottom1] = xywh2xyxy(cx1, cy1, w1, h1);
   area1 = w1 * h1;
 
-  left2 = Math.round(cx2 - w2/2.0);
-  right2 = Math.round(cx2 + w2/2.0);
-  top2 = Math.round(cy2 - h2/2.0);
-  bottom2 = Math.round(cy2 + h2/2.0);
+  let [left2, right2, top2, bottom2] = xywh2xyxy(cx2, cy2, w2, h2);
   area2 = w2 * h2;
 
   /* interaction */
@@ -76,11 +63,7 @@ function iou(cx1, cy1, w1, h1, cx2, cy2, w2, h2) {
   intersection_area = iw * ih;
 
   union_area = area1 + area2 - intersection_area;
-  ret = intersection_area / union_area;
-  console.log(left1, right1, top1, bottom1);
-  console.log(left2, right2, top2, bottom2, ret);
-  console.log(intersection_area, union_area);
-  return ret;
+  return intersection_area / union_area;
 }
 
 const YOLOV5S_BOXES = 25200;
@@ -92,56 +75,52 @@ async function non_max_suppression(input) {
   var candidates = [];
   for (var idx = 0; idx < YOLOV5S_BOXES; idx++) {
     var arr = input.subarray(idx * YOLOV5S_CLASSES, (idx+1) * YOLOV5S_CLASSES);
-    max_cls_conf = 0;
-    max_cls = null;
-    for (var jdx = 5; jdx < YOLOV5S_CLASSES; jdx++) {
-      if (arr[jdx] > max_cls_conf) {
-        max_cls_conf = arr[jdx];
-        max_cls = jdx-5;
-      }
-    }
-    //arr[4] = arr[4] * max_cls_conf;
-    if (arr[4] > CONF_THRES) {
+    if (arr[CLS_CONF_IDX] > CONF_THRES) {
       candidates.push(arr);
-      console.log(max_cls);
     }
   }
-  candidates = candidates.sort((a, b) => b[4] - a[4]); /* sort by score */
-  console.log("candidates:", candidates);
+  /* use Non Max Suppression to get 'selected' */
+  candidates = candidates.sort((a, b) => b[CLS_CONF_IDX] - a[CLS_CONF_IDX]); /* sort by score */
   var selected = [];
   candidates.forEach(cand => {
-    xywh2xyxy(cand[0], cand[1], cand[2], cand[3]);
     let add = true;
     for (var idx = 0; idx < selected.length; idx++) {
       const val = iou(cand[0], cand[1], cand[2], cand[3], selected[idx][0], selected[idx][1], selected[idx][2], selected[idx][3]);
-      console.log(val);
       if (val > IOU_THRES) {
         add = false;
-        console.log("add false");
       }
     }
 
     if (add) {
-      console.log("add");
       selected.push(cand);
     }
-  })
-  console.log(selected);
+  });
+
+  /* get simplified output from 'selected' */
+  var answer = [];
+  selected.forEach(sel => {
+    max_cls_conf = 0;
+    max_cls = null;
+    for (var idx = 5; idx < YOLOV5S_CLASSES; idx++) {
+      if (sel[idx] > max_cls_conf) {
+        max_cls_conf = sel[idx];
+        max_cls = idx-5;
+      }
+    }
+    let [x1, x2, y1, y2] = xywh2xyxy(Math.round(sel[0]), Math.round(sel[1]), Math.round(sel[2]), Math.round(sel[3]))
+    answer.push([x1, x2, y1, y2, max_cls]);
+  });
+  console.log(answer);
 }
 
 async function main() {
-
-  console.log("Start");
-
-  inkjet.decode(fs.readFileSync("./bluejay640.jpg"), function(err, decoded) {
+  inkjet.decode(fs.readFileSync("./bcd.jpg"), function(err, decoded) {
     if (decoded != undefined) {
       modelInput = new Float32Array(modelHeight*modelWidth*3);
       preProcess(decoded, modelInput);
 
       tensor = new ort.Tensor("float32", modelInput, [1, 3, modelHeight, modelWidth]);
-      console.log("go", tensor);
       ort.InferenceSession.create("./yolov5m.onnx").then((se) => {
-        console.log("session", se.inputNames);
         se.run({"images": tensor}).then((res) => {
           non_max_suppression(res.output.data);
         });
